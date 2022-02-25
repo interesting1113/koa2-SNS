589b356860252754d9557752ce0e89a7a0c99a03
sylviayang <sylviayang1113@gmail.com>
2022-02-25 11:12:33 +0900

    连接池

--- sequelize-test/seq.js
+++ sequelize-test/seq.js
@@ -5,6 +5,12 @@ const conf = {
   dialect: 'mysql'
 }
 
+conf.pool = {
+  max: 5, // 连接池中最大的连接数量
+  min: 3, // 最小
+  idle: 10000 // 如果一个线程 10s 之内没有被使用则释放
+}
+
 const seq = new Sequelize('koa2_weibo', 'root', '123456', conf)
 
 module.exports = seq
