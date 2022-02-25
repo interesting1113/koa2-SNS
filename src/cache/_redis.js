7be0c42dd238033be23f27b33a506b87a1e50065
sylviayang <sylviayang1113@gmail.com>
2022-02-25 11:54:18 +0900

    redis: node操作redis

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -12,6 +12,48 @@ redisClient.on('error', err => {
   console.log('redis error', err)
 })
 
-// set
+/**
+ * redis set
+ * @param {string} key key
+ * @param {string} val val
+ * @param {number} timeout expire s
+ */
+function set(key, val, timeout = 60 * 60) {
+  if (typeof val === 'object') {
+    val = JSON.stringify(val)
+  }
+  redisClient.set(key, val)
+  redisClient.expire(key, timeout)
+}
 
-// get
+/**
+ * redis get
+ * @param {string} key key
+ */
+function get(key) {
+  const promise = new Promise((resolve, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      if (val == null) {
+        resolve(null)
+        return 
+      }
+      try {
+        resolve(
+          JSON.parse(val)
+        )
+      } catch (ex) {
+        resolve(val)
+      }
+    })
+  })
+  return promise
+}
+
+module.exports = {
+  set,
+  get
+}
