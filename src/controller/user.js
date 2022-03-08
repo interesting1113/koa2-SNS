285258de78a354f2e8181d285f6467c4f0099eb4
sylviayang <sylviayang1113@gmail.com>
2022-03-08 11:02:03 +0900

    feat: 开发register

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,10 +3,11 @@
  * @author sylviayang
  */
 
-const { getUserInfo } = require("../services/user")
+const { getUserInfo, createUser } = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const {
-  registerUserNameNotExistInfo
+  registerUserNameNotExistInfo,
+  registerUserNameExistInfo
 } = require('../model/ErrorInfo')
 /**
  * 用户名是否存在
@@ -22,6 +23,34 @@ async function isExist(userName) {
   }
 }
 
+/**
+ * 注册
+ * @param {string} userName 用户名
+ * @param {string} password 密码
+ * @param {number} gender 性别 （1 男， 2 女， 3 保密）
+ */
+async function register({ userName, password, gender }) {
+  const userInfo = await getUserInfo(userName)
+  if (userInfo) {
+    // 用户名已存在
+    return new ErrorModel(registerUserNameExistInfo)
+  }
+
+  // 注册 service
+  try {
+    await createUser({
+      userName,
+      password,
+      gender 
+    })
+    return new SuccessModel()
+  } catch (ex) {
+    console.log(ex.message, ex.stack)
+    return new ErrorModel(registerFailInfo)
+  }
+}
+
 module.exports = {
-  isExist
+  isExist,
+  register
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,13 +3,19 @@
  */
 
 const router = require('koa-router')()
-const { isExist } = require('../../controller/user')
+const { isExist, register } = require('../../controller/user')
+const { TEXT } = require('../../db/types')
 
 router.prefix('/api/user')
 
 // 注册路由
 router.post('/register', async (ctx, next) => {
-
+  const { userName, password, gender } = ctx.request.body
+  ctx.body = await register ({
+    userName,
+    password,
+    gender
+  })
 })
 
 // 用户名是否存在
