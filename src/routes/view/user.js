e51218dbbae1fa5be365026b72ca5e5c73c0b457
sylviayang <sylviayang1113@gmail.com>
2022-03-11 12:12:11 +0900

    fix: 判断一登陆情况下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,30 @@
 
 const router = require('koa-router')()
 
+/**
+ * 
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo(ctx) {
+  const data = {
+    isLogin: false // 默认未登陆
+  }
+
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    let data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+}
+
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register')
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
