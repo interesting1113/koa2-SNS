bdb6147af426c827d22b5b6c0ae3a84bc34fa217
sylviayang <sylviayang1113@gmail.com>
2022-03-16 21:06:49 +0900

    feat: 修改基本信息API

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,13 +3,18 @@
  * @author sylviayang
  */
 
-const { getUserInfo, createUser, deleteUser } = require('../services/user')
+const { 
+  getUserInfo, 
+  createUser, 
+  deleteUser, 
+  updateUser } = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const {
   registerUserNameNotExistInfo,
   registerUserNameExistInfo,
   loginFailInfo,
-  deleteUserFailInfo
+  deleteUserFailInfo,
+  changeInfoFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -89,9 +94,44 @@ async function deleteCurUser(userName) {
   return new ErrorModel(deleteUserFailInfo)
 }
 
+/**
+ * 修改个人信息
+ * @param {Object} ctx 
+ * @param {string} nickName 
+ * @param {string} city
+ * @param {string} picture
+ */
+async function changeInfo(ctx, { nickName, city, picture }) {
+  const { userName } = ctx.session.userInfo
+  if (!nickName) {
+    nickName = userName
+  }
+  const result = await updateUser(
+    {
+      newNickName: nickName,
+      newCity: city,
+      newPicture: picture
+    },
+    { userName }
+    )
+    if (result) {
+      // 执行成功
+      Object.assign(ctx.session.userInfo, {
+        nickName,
+        city,
+        picture
+      })
+      // 执行成功的返回
+      return new SuccessModel()
+    }
+    // 失败
+    return new ErrorModel(changeInfoFailInfo)
+}
+
 module.exports = {
   isExist,
   register,
   login,
-  deleteCurUser
+  deleteCurUser,
+  changeInfo
 }
