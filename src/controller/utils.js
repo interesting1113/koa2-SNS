2b3a2534ec72723c931f5eb99accb7b8bbdd7bd6
sylviayang <sylviayang1113@gmail.com>
2022-03-16 20:30:02 +0900

    feat: 上传图片API

--- src/controller/utils.js
+++ src/controller/utils.js
@@ -3,11 +3,22 @@
  * @author sylviayang
  */
 
-const { ErrorModel } = require("../model/ResModel")
-const { uploadFileSizeFailInfo } = require("../model/ErrorInfo")
+const path = require('path')
+const { ErrorModel, SuccessModel } = require('../model/ResModel')
+const { uploadFileSizeFailInfo } = require('../model/ErrorInfo')
 const fse = require('fs-extra')
 
+// 存储目录
+const DIST_FOLDER_PATH = path.join(__dirname, '..', '..', 'uploadFiles')
+// 文件最大体积 1M
 const MAX_SIZE = 1024 * 1024 * 1024
+
+// 是否需要创建目录
+fse.pathExists(DIST_FOLDER_PATH).then(exist => {
+  if (!exist) {
+    fse.ensureDir(DIST_FOLDER_PATH)
+  }
+})
 
 /**
  * 保存文件
@@ -22,6 +33,17 @@ async function saveFile({ name, type, size, filePath }) {
     await fse.remove(filePath)
     return new ErrorModel(uploadFileSizeFailInfo)
   }
+
+  // 移动文件
+  const fileName = Date.now() + '.' + name // 防止重名
+  const distFilePath = path.join(DIST_FOLDER_PATH, fileName)
+  await fse.move(filePath, distFilePath)
+
+  // 返回信息
+  return new SuccessModel({
+    url: '/' + fileName
+  })
+  
 }
 
 module.exports = {
