87bc8ddaa6ad14d7968e3c575b5b7ac1720d116a
sylviayang <sylviayang1113@gmail.com>
2022-03-22 19:21:49 +0900

    feat: 退出登录

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -9,7 +9,8 @@ const {
   login,
   deleteCurUser,
   changeInfo,
-  changePassword 
+  changePassword,
+  logout 
 } = require('../../controller/user')
 const userValidate = require('../../validator/user')
 const { genValidator } = require('../../middlewares/validator')
@@ -55,10 +56,15 @@ router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, 
   ctx.body = await changeInfo(ctx, { nickName, city, picture })
 })
 
+// 修改密码
 router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
   const { password, newPassword } = ctx.request.body
   const { userName } = ctx.session.userInfo
   ctx.body = await changePassword(userName, password, newPassword)
 })
 
+// 退出登录
+router.post('/logout', loginCheck, async (ctx, next) => {
+  ctx.body = await logout(ctx)
+})
 module.exports = router
