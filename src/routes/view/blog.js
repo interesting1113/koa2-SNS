10a7276afc36283523b889e85b721d6094d4274e
interesting1113 <sylviayang1113@gmail.com>
2022-05-13 19:55:22 +0900

    开始和创建页面

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -11,4 +11,15 @@ router.get('/', loginRedirect, async (ctx, next) => {
   await ctx.render('index', {})
 })
 
+// 个人主页
+router.get('/profile', loginRedirect, async (ctx, next) => {
+  const { userName } = ctx.session.userInfo
+  ctx.redirect(`/profile/${userName}`)
+})
+router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
+  await ctx.render('profile', {
+
+  })
+})
+
 module.exports = router
