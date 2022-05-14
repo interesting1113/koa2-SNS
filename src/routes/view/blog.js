d0ac291d4a13f998a50392237247bb06b41b592e
interesting1113 <sylviayang1113@gmail.com>
2022-05-14 15:56:42 +0900

    创建路由-用户数据

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -6,6 +6,7 @@
 const router = require('koa-router')()
 const { loginRedirect } = require('../../middlewares/loginChecks')
 const { getProfileBlogList } = require('../../controller/blog-profile')
+const { isExist } = require('../../controller/user')
 
 // 首页
 router.get('/', loginRedirect, async (ctx, next) => {
@@ -18,8 +19,24 @@ router.get('/profile', loginRedirect, async (ctx, next) => {
   ctx.redirect(`/profile/${userName}`)
 })
 router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
-
+  // 已登陆用户的信息
+  const myUserInfo = ctx.session.userInfo
+  const myUserName = myUserInfo.userName
+  
+  let curUserInfo
   const { userName: curUserName } = ctx.params
+  const isMe = myUserName === curUserName
+  if (isMe) {
+    // 是当前登陆用户
+    curUserInfo = myUserInfo
+  } else {
+    // 不是当前登陆用户
+    const existResult = await isExist(curUserName)
+    // 用户名不存在
+    if (existResult.errno !== 0) return
+    // 用户名存在
+    curUserInfo = existResult.data
+  }
   // 获取第一页数据 
   const result = await getProfileBlogList(curUserName, 0)
   const { isEmpty, blogList, pageSize, pageIndex, count } = result.data
@@ -31,6 +48,10 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
       pageSize,
       pageIndex,
       count
+    },
+    userData: {
+      userInfo: curUserInfo,
+      isMe
     }
   })
 })
