c8df85bbbd30b3a7e97dbee8a179322040ad7a11
interesting1113 <sylviayang1113@gmail.com>
2022-05-19 16:15:26 +0900

    使用redux管理详情页面数据

--- src/routes/api/blog-square.js
+++ src/routes/api/blog-square.js
@@ -3,22 +3,22 @@
  * @author sylviayang
  */
 
- const router = require('koa-router')
- const { loginCheck } = require('../../middlewares/loginChecks')
- const { getProfileBlogList } = require('../../controller/blog-profile')
- const { getBlogListStr } = require('../../utils/blog')
- 
- router.prefix('/api/square')
- 
- // 加载更多
- router.get('/loadMore/:pageIndex', loginCheck, async(ctx, next) => {
-   const { userName, pageIndex } = ctx.params
-   pageIndex = parseInt(pageIndex)
-   const result = await getProfileBlogList(userName, pageIndex)
+const router = require('koa-router')
+const { loginCheck } = require('../../middlewares/loginChecks')
+const { getProfileBlogList } = require('../../controller/blog-profile')
+const { getBlogListStr } = require('../../utils/blog')
 
-   result.data.blogListTpl = getBlogListStr(result.data.blogList)
+router.prefix('/api/square')
+
+// 加载更多
+router.get('/loadMore/:pageIndex', loginCheck, async(ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  pageIndex = parseInt(pageIndex)
+  const result = await getProfileBlogList(userName, pageIndex)
+
+  result.data.blogListTpl = getBlogListStr(result.data.blogList)
 
   ctx.body = result
- })
- 
- module.exports = router
+})
+
+module.exports = router
