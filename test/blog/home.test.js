5851336111fbc1de0dec2540d2d77fba0ffe07fe
ヨウ・セキ <interesting1113@yousekideMacBook-Pro.local>
2022-06-12 12:15:17 +0900

    square unit test

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -13,12 +13,12 @@ test('创建一条微博，应该成功', async () => {
   const image = '/xxx.png'
 
   const res = await server
-    .post('/api/blog/create')
-    .send({
-      content,
-      image
-    })
-    .set('cookie', COOKIE)
+              .post('/api/blog/create')
+              .send({
+                content,
+                image
+              })
+              .set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
   expect(res.body.data.content).toBe(content)
   expect(res.body.data.image).toBe(image)
--- test/blog/square.test.js
+++ test/blog/square.test.js
@@ -1,16 +1,16 @@
 /**
- * @description 个人主页test
+ * @description 广场test
  * @author sylviayang
  */
 
  const server = require('../server')
- const { USER_NAME_COOKIE, COOKIE } = require('../testUserInfo') 
+ const { COOKIE } = require('../testUserInfo') 
 
 
- test('个人主页，加载第一页数据应该成功', async () => {
+ test('广场页，加载第一页数据', async () => {
    const res = await server
-    .get(`/api/profile/loadMore/${USER_NAME}/0`)
-    .set('cookie', COOKIE)
+                .get(`/api/profile/loadMore/0`)
+                .set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
 
   const data = res.body.data
