e565c483774a3f3a5f818a4d92a4ef398a380ed2
interesting1113 <sylviayang1113@gmail.com>
2022-06-26 12:06:48 +0900

    首页数据建模

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -17,7 +17,10 @@ UserRelation.belongsTo(User, {
 User.hasMany(UserRelation, {
   foreignKey: 'userId'
 })
-
+Blog.belongsTo(UserRelation, {
+  foreignKey: 'userId',
+  targetKey: 'followerId'
+})
 
 module.exports = {
   User,
