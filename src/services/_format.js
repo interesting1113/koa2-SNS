ceb39b3a10ea4baa6353af8abbbd475d00bbbdbd
interesting1113 <sylviayang1113@gmail.com>
2022-07-01 13:53:47 +0900

    at用户转为链接形式

--- src/services/_format.js
+++ src/services/_format.js
@@ -3,7 +3,7 @@
  * @author sylviayang
  */
 
-const { DEFAULT_PICTURE } = require('../conf/constant')
+const { DEFAULT_PICTURE, REG_FOR_AT_WHO } = require('../conf/constant')
 const {timeFormat} = require('../utils/dt')
 /**
  * 用户默认头像
@@ -47,15 +47,39 @@ function _formatDBTime(obj) {
 }
 
 /**
+ * 
+ * @param {Object} obj 微博数据对象 
+ */
+function _formatContent(obj) {
+  obj.contentFormat = obj.content
+
+  // 格式化 @
+  // from '哈喽 @张三 - zhangsan 你好'
+  // to '哈喽 <a href="/profile/zhangsan">张三</a> 你好'
+  obj.contentFormat = obj.contentFormat.replace(
+    REG_FOR_AT_WHO,
+    (matchStr, nickName, userName) => {
+      return `<a href="/profile/${userName}">@${nickName}</a>`
+    }
+  )
+
+  return obj
+}
+
+/**
  * 格式化微博信息
  * @param {Array|Object} list 
  */
 function formatBlog(list) {
   if (list == null) return
   if (list instanceof Array) {
-    return list.map(_formatDBTime)
+    return list.map(_formatDBTime).map(_formatContent)
   }
-  return _formatDBTime (list)
+  // 对象
+  let result = list 
+  result = _formatDBTime (list)
+  result = _formatContent(result)
+  return result
 }
 
 module.exports = {
